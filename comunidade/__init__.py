4b470099f429d2c179a5911d45b8fe54242cf939
italosoousa <italomadeira21@gmail.com>
2025-09-16 11:58:13 -0300

    Modificando o banco de dados

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 # Criando um token para previnir ataques 
 app.config['SECRET_KEY'] = '2f29ddb0e4ab691fb698426797'
-# Onde vai ficar o banco de dados no aplicativo
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+# Criando o banco de dados, para que ele também use a versão local
+if os.getenv('DATABASE_URL'):
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app) # banco de dados
 bcrypt = Bcrypt(app) # crypto da senha
